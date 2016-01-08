ca58d0a8810763816782a6966fb0d905072e29ee
Jack Reed <pjreed@stanford.edu>
2016-01-08 15:06:56 -0500

    update Blacklight named path helpers

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Geoblacklight::Engine.routes.draw do
   resources :download, only: [:show, :file]
   get 'download/file/:id' => 'download#file', as: :download_file
   get 'download/hgl/:id' => 'download#hgl', as: :download_hgl
-  get 'catalog/:id/web_services' => 'catalog#web_services', as: 'web_services_catalog'
-  get 'catalog/:id/metadata' => 'catalog#metadata', as: 'metadata_catalog'
+  get 'catalog/:id/web_services' => 'catalog#web_services', as: 'web_services_solr_document'
+  get 'catalog/:id/metadata' => 'catalog#metadata', as: 'metadata_solr_document'
 end
