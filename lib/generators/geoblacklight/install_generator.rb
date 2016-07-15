a04def7ed6ec46df9c83242155ce614a4b5170cf
Jack Reed <phillipjreed@gmail.com>
2016-07-15 13:29:51 -0400

    use geoblacklight.scss

--- lib/generators/geoblacklight/install_generator.rb
+++ lib/generators/geoblacklight/install_generator.rb
@@ -32,7 +32,7 @@ module Geoblacklight
     end
 
     def assets
-      copy_file 'geoblacklight.css.scss', 'app/assets/stylesheets/geoblacklight.css.scss'
+      copy_file 'geoblacklight.scss', 'app/assets/stylesheets/geoblacklight.scss'
       copy_file 'geoblacklight.js', 'app/assets/javascripts/geoblacklight.js'
 
       append_to_file 'config/initializers/assets.rb',
