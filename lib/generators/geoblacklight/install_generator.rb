a159de3bad38ad7568910021b732c413b0827fa4
Jack Reed <phillipjreed@gmail.com>
2016-07-15 12:08:27 -0400

    temporary fix to engine not mounting routes

--- lib/generators/geoblacklight/install_generator.rb
+++ lib/generators/geoblacklight/install_generator.rb
@@ -12,7 +12,14 @@ module Geoblacklight
     desc 'Install Geoblacklight'
 
     def inject_geoblacklight_routes
-      route "mount Geoblacklight::Engine => 'geoblacklight'"
+      route <<-EOF.strip_heredoc
+          post 'wms/handle'
+          resources :download, only: [:show, :file]
+          get 'download/file/:id' => 'download#file', as: :download_file
+          get 'download/hgl/:id' => 'download#hgl', as: :download_hgl
+          get 'catalog/:id/web_services' => 'catalog#web_services', as: 'web_services_solr_document'
+          get 'catalog/:id/metadata' => 'catalog#metadata', as: 'metadata_solr_document'
+      EOF
     end
 
     def install_jettywrapper
